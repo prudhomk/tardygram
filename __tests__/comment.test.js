192714ac89fb3c639ae9b5767126156d09919d18
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-30 14:38:54 -0700

    comment post test pass

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -3,17 +3,17 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
-import { post } from 'superagent';
-
+import Post from '../lib/models/Post.js';
 
 describe('demo routes', () => {
 
   let user = {}; 
+  let post = {};
   let agent;
   
   beforeEach(async() => {
     await setup(pool);
-    agent = await request.agent(app);
+    agent = request.agent(app);
     user = await UserService.create({
       username: 'Billy',
       password: 'password',
@@ -24,11 +24,22 @@ describe('demo routes', () => {
         username: 'Billy',
         password:'password'
       });
+   
       
+    
   });
-
+  
   it('creates a comment via POST', async() => {
+   
+    post = await Post.insert({
+      userId: user.id,
+      photoUrl: 'picture',
+      caption: 'Look at this!',
+      tags: ['selfie', 'summer']
+    });
 
+ 
+  
     const res = await agent
       .post('/api/v1/comments')
       .send({
@@ -36,7 +47,7 @@ describe('demo routes', () => {
         post: post.id,
         comment: 'Wow!',
       });
-
+ 
     expect(res.body).toEqual({
       id: '1',
       commentBy: user.id,
